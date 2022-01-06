bdcb176a9ffdb36aeaba2916131bee4b2ee5199e
Siniavskay-Nadejda <87280720+Siniavskay-Nadejda@users.noreply.github.com>
2022-01-06 13:11:39 +0200

    изменения

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ function renderList(countries) {
     .map(({ name, flags }) => {
       return `
           <li class="country-list__item">
-               <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
+              <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
               <h2 class="country-list__name">${name.official}</h2>
           </li>
           `
